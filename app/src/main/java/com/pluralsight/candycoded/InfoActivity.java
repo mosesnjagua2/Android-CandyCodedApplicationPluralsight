028ac8fe52dc5eb84cf929507c354dc46b0a3ffe
Jayd <aimejayd2012@gmail.com>
2019-11-01 10:30:29 +0200

    Android-CandyCodedApplicationPluralsight

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 import android.view.View;
+import android.widget.ImageView;
+
 import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
@@ -16,7 +17,7 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
-        ImageView candyStoreImageView = (ImageView) findViewById(R.id.image_view_candy_store);
+        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
@@ -24,19 +25,26 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+    public void createMapIntent(View view){
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager())!= null){
+            startActivity(mapIntent);
+        }
+    }
+
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
+    }
+
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-    public void createMapIntent(View view)
-    {
 
-        Uri uriaddres = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriaddres);
-        mapIntent.setPackage("com.google.android.app.maps");
-        if (mapIntent.resolveActivity(getPackageManager()) !=null){
-            startActivity(mapIntent);
-        }
-    }    // ***
+    // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
 }
